2664fcb1e09441d5f30eedce3f909de5bf95334d
GiselleMM <giselle.magalong@gmail.com>
2022-09-07 12:10:28 +0200

    ADD signIn and register

--- src/pages/PageComponents/components/Sign.svelte
+++ src/pages/PageComponents/components/Sign.svelte
@@ -1,59 +1,107 @@
 <script>
-  let bgc = "#ffffff";
-  let textColor = "#000000";
-  let size = "28px";
+  let bgc = "#56a1f0";
+  let textColor = "#f2eded";
   let borderRadius = "2px";
-  let border="1px solid";
 </script>
 
 <div id="compo-container">
     <div id="preview">
-        <div id="formDiv">
-            <h2 id="titre" style="color: {textColor}">Formulaire</h2>
-            <form id="signIn" method="post" action="" onsubmit="">
-                <label for="">Nom</label>
-                <input
-                        name="name"
-                        type="text"
-                />
-                <label for="">Prénom</label>
-                <input
-                        name="firstName"
-                        type="text"
-                />
-                <label for="">Mot de passe</label>
-                <input
-                        type="password"
-                />
-                <input id="inputSubmit" type="submit" value="Envoyer"/>
+        <div id="modal" style="border-radius: {borderRadius}">
+            <h2 id="titre" style="color: {bgc}">Sign in</h2>
+            <form action="">
+                <div id="input-group">
+                    <label for="">Email</label>
+                    <input type="email" placeholder="Enter Email"/>
+                    <label for="">Password</label>
+                    <input type="email" placeholder="Password"/>
+                </div>
+                <div id="option">
+                    <div id="checkbox">
+                        <input type="checkbox">
+                        <label for="">Remember me</label>
+                    </div>
+                    <a href="#" style="color: {bgc}">Forgot password?</a>
+                </div>
+                <button type="submit" style="background-color: {bgc}; color:{textColor}; border-radius:{borderRadius}">Sign in</button>
+                <p>Not a member? <a href="#" style="color: {bgc}">Register</a></p>
             </form>
         </div>
     </div>
     <div id="code-sources">
         <h2>Html</h2>
         <p>
-            &lt;label&gt; Mon select &lt;label&gt;<br/>
-            &lt;select style="border: none; background-color: {bgc}; color: {textColor};
-            font-size: {size}; border-radius: {borderRadius}; border:{border}"&gt;  <br />&#160;
-            &lt;option value="value1"&gt; Value 1 &lt;/option&gt;<br />&#160;
-            &lt;option value="value2"&gt; Value 2 &lt;/option&gt; <br />&#160;
-            &lt;option value="value3"&gt; Value 3 &lt;/option&gt; <br />
-            &lt;/select&gt;
+            &lt;div id="modal" style="border-radius:{borderRadius};width:300px, border: 1px solid; padding:30px&gt;<br />&#160;
+            &lt;h2&gt; Sign in &lt;/h2&gt; <br />&#160;
+            &lt;form action=""&gt;<br />&#160; &#160;
+
+            &lt;div id="input-group" style="display:flex; flex-direction: column; margin-bottom:5px"&gt; <br />&#160; &#160; &#160;
+            &lt;label name="" style="margin-bottom:10px; height: 20px;" &gt; Email &lt;/label&gt;<br />&#160; &#160; &#160;
+            &lt;input type="email" placeholder="Enter Email" style="margin-bottom:10px; height: 20px;" /&gt;<br />&#160; &#160; &#160;
+            &lt;label name="" style="margin-bottom:10px; height: 20px;" &gt; Password &lt;/label&gt;<br />&#160; &#160; &#160;
+            &lt;input type="password" placeholder="Enter password" style="margin-bottom:10px; height: 20px;" /&gt;<br />&#160; &#160;
+            &lt;/div&gt<br/>&#160; &#160;
+
+            &lt;div id="option" style="display:flex, justify-content:space-between; align-items:center;<br/>&#160; &#160; margin-bottom:10px"&gt;<br/>&#160; &#160; &#160;
+            &lt;div id="checkbox"&gt;<br/>&#160; &#160; &#160; &#160;
+            &lt;input type="checkbox" style="margin-bottom:10px; height: 20px;" &gt;<br/>&#160; &#160; &#160; &#160;
+            &lt;label style="margin-bottom:10px; height: 20px;" name="" &gt; Remember me &lt;/label&gt;<br/>&#160; &#160; &#160;
+            &lt;/div&gt;<br/>&#160; &#160; &#160;
+            &lt;a href="#" style="color:{bgc};" &gt; Forgot password? &lt;/a&gt; <br/>&#160; &#160;
+            &lt;/div&gt;<br/> &#160;&#160;
+
+            &lt;button type="submit" style="background-color:{bgc}; color: {textColor}; width:300px; height:30px; border: none" &gt;&lt;/button&gt;<br>&#160; &#160;
+            &lt;p&gt;Not a member?&lt;a href="#" style="color:{bgc}&gt;  Register&lt;/a&gt;&lt;/p&gt;<br>&#160;
+            &lt;/form&gt;<br />
+            &lt;/div&gt;
         </p>
         <h2>Svelte</h2>
         <p>
-            &lt;label&gt;Mon select&lt;/label&gt; <br />
-            &lt;select&gt;<br />&#160;
-            &lt;option value="value1"&gt; Value 1 &lt;/option &gt; <br /> &#160;
-            &lt;option value="value1"&gt; Value 1 &lt;/option &gt; <br /> &#160;
-            &lt;option value="value1"&gt; Value 1 &lt;/option &gt; <br />
-            &lt;/select&gt; <br/> <br/>
+            &lt;div id="modal" &gt;<br />&#160;
+            &lt;h2 id="titre" &gt; Sign in &lt;/h2&gt; <br />&#160;
+            &lt;form action=""&gt;<br />&#160; &#160;
 
-            &lt;style&gt; <br /> select &#123; <br />&#160; border: {border}; <br />&#160;
-            background-color: {bgc}; <br />&#160; color: {textColor}; <br />&#160;
-            font-size: {size};
-            <br />&#160; border-radius: {borderRadius}; <br />&#160; padding: 4px 15px
-            <br />margin-top: 5px; <br />&#160;width: 200px; <br />&#160;
+            &lt;div id="input-group" &gt; <br />&#160; &#160; &#160;
+            &lt;label name="" &gt; Email &lt;/label&gt;<br />&#160; &#160; &#160;
+            &lt;input type="email" placeholder="Enter Email" /&gt;<br />&#160; &#160; &#160;
+            &lt;label name="" &gt; Password &lt;/label&gt;<br />&#160; &#160; &#160;
+            &lt;input type="password" placeholder="Enter password" /&gt;<br />&#160; &#160;
+            &lt;/div&gt<br/>&#160; &#160;
+
+            &lt;div id="option"&gt;<br/>&#160; &#160; &#160;
+            &lt;div id="checkbox"&gt;<br/>&#160; &#160; &#160; &#160;
+            &lt;input type="checkbox" &gt;<br/>&#160; &#160; &#160; &#160;
+            &lt;label &gt; Remember me &lt;/label&gt;<br/>&#160; &#160; &#160;
+            &lt;/div&gt;<br/>&#160; &#160; &#160;
+            &lt;a href="#" &gt; Forgot password? &lt;/a&gt; <br/>&#160; &#160;
+            &lt;/div&gt;<br/> &#160;&#160;
+
+            &lt;button type="submit" &gt;Sign in&lt;/button&gt;<br>&#160; &#160;
+            &lt;p&gt;Not a member?&lt;a href="#" &gt;  Register&lt;/a&gt;&lt;/p&gt;<br>&#160;
+            &lt;/form&gt;<br />
+            &lt;/div&gt; <br> <br>
+
+            &lt;style&gt; <br />
+            #modal &#123; <br />&#160;
+            border-radius: {borderRadius}; <br />&#160; width: 300px; <br />&#160;
+            border: 1px solid; <br />&#160; padding: 30px; <br />
+            &#125; <br>
+            #titre &#123; <br />&#160;
+            color: {bgc}; <br />&#160; <br />
+            &#125;<br>
+            #input-group &#123; <br />&#160;
+            display: flex; <br />&#160; flex-direction: column; <br />&#160; margin-bottom : 5px; <br />&#160;<br />
+            &#125;<br>
+            label, input &#123; <br />&#160;
+            margin-bottom : 10px ; <br />&#160; height : 20px ; <br />&#160;<br />
+            &#125;<br>
+            #option &#123; <br />&#160;
+            display : flex ; <br />&#160; justify-content : space-between ; <br />&#160; align-items : center ; <br /> &#160; margin-bottom: 10px ;<br />
+            &#125;<br>
+            button &#123; <br />&#160;
+            width : 300px ; <br/>&#160; height : 30px; <br/>&#160; border : none;<br/>&#160; color : {textColor};<br/>&#160; background-color : {bgc};<br/>
+            &#125;<br>
+            a &#123; <br />&#160;
+            color : {bgc} ;<br />
             &#125;
             <br />&lt;/style&gt;
         </p>
@@ -76,16 +124,6 @@
     </div>
 
     <div>
-        <label for="">Size</label>
-        <select value={size} on:input={(e) => (size = e.target.value)}>
-            <option value="12px">sm</option>
-            <option value="20px">md</option>
-            <option value="28px">lg</option>
-            <option value="36px">xl</option>
-        </select>
-    </div>
-
-    <div>
         <label for="">Border Radius</label>
         <select
                 value={borderRadius}
@@ -100,20 +138,6 @@
         </select>
     </div>
 
-    <div>
-        <label for="">Border Style</label>
-        <select
-                value={border}
-                on:input={(e) => (border = e.target.value)}
-        >
-            <option value="1px solid">Default</option>
-            <option value="none">None</option>
-            <option value="1px dashed">Dashed</option>
-            <option value="1px dotted">Dotted</option>
-            <option value="double">Double</option>
-            <option value="1px groove">Groove</option>
-        </select>
-    </div>
 </div>
 
 <style>
@@ -136,24 +160,11 @@
         align-items: center;
     }
 
-    #signIn{
-        display: flex;
-        flex-direction: column;
-        width: 400px;
-    }
-
-    #inputSubmit{
-        margin-top: 20px;
-    }
-
-    label{
-        margin-top: 10px;
-        margin-bottom: 5px;
-    }
-
     #code-sources {
         background-color: rgba(208, 208, 208, 0.341);
         border-radius: 5px;
+        max-height: 500px;
+        overflow: scroll;
         min-height: 300px;
         padding: 0 20px;
     }
@@ -163,6 +174,36 @@
         gap: 30px;
         padding: 20px 90px;
         justify-content: center;
+    }
+
+    #modal{
+        width: 300px;
+        border: 1px solid;
+        padding: 30px;
+    }
+
+    #input-group{
+        display: flex;
+        flex-direction: column;
+        margin-bottom: 5px;
+    }
+
+    label, input{
+        margin-bottom: 10px;
+        height: 20px;
+    }
+
+    #option{
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 10px;
+    }
+
+    button{
+        width: 300px;
+        height: 30px;
+        border:none;
     }
 
 </style>
